9cec94bcbbfa6b0fbd691f15039d029d8c9c5ebd
Tina Truong <tina.truong21@gmail.com>
2016-03-23 14:29:37 -0400

    made formatting changes

--- exercise6.rb
+++ exercise6.rb
@@ -5,10 +5,9 @@ grocery_list = ["candy","chocolate","chips","cupcakes","salmon"]
 #1. adding rice to grocery list and show grocery list 
 grocery_list << "rice"
 
-def show_grocery_list (list)
+def show_grocery_list(list)
 	list.each { |x| puts "*" + x }
 end 
-
 
 # 2. output # of items in list
 puts grocery_list.length
