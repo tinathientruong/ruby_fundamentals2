c1f22cba499d58b2d55171cdfac6653f4ef97872
Tina Truong <tina.truong21@gmail.com>
2016-03-22 23:15:38 -0400

    completed exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,12 @@
+puts "what is the temperature (in Fahrenheit)?"
+userInput = gets.chomp.to_i
+
+
+def change_temp (f) 
+   return (f-32) * 5/9
+end 
+
+converted_temp = change_temp(userInput)
+
+puts "The converted temperature in Celsius is #{converted_temp} "
+
